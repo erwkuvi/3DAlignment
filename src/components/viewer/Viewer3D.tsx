5c34eab071fc45e4688c729e6790ac6c1766cc75
Erwin K <e.kuechel@gmail.com>
2025-11-11 21:55:50 +0100

    project update

---
+++ src/components/viewer/Viewer3D.tsx
@@ -0,0 +1,10 @@
+import { useEffect, useRef } from 'react';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+function Viewer3D() {
+    const mountRef = useRef<HTMLDivElement>(null);
+    const sceneRef = useRef<THREE.Scene>();
+    const cameraRef = useRef<THREE.PerspectiveCamera>();
+    const rendererRef = useRef<THREE.WebGLRenderer>();
+}
